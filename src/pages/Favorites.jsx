e60d69336ee0506748fe984b315994d64d0f15ca
AshMargaryan <margaryanashot279@gmail.com>
2026-05-23 19:05:49 +0400

    Routing Implemented

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,11 @@
+function Favorites() {
+    return (
+        <div className="favorites-empty">
+            <h1>Favorites</h1>
+            <h2>No favorite movies yet.</h2>
+            <p>Start adding movies to your favorites list!</p>
+        </div>
+    )
+}
+
+export default Favorites
